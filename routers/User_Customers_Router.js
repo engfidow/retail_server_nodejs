7fdb3d1707cc2cb8db9ae13bbf4e2ba7cf23c863
engfidow <abdallayusufahmed@gmail.com>
2024-05-07 11:03:30 +0300

    first commit

---
+++ routers/User_Customers_Router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userCustomersController = require('../controllers/User_Customers_Controller');
+const router = express.Router();
+
+// Routes for User Customers
+router.post('/userCustomers', userCustomersController.createUserCustomer);
+router.get('/userCustomers', userCustomersController.getAllUserCustomers);
+router.get('/userCustomers/email/:email', userCustomersController.getUserCustomerByEmail);
+router.patch('/userCustomers/:id', userCustomersController.updateUserCustomer);
+router.delete('/userCustomers/:id', userCustomersController.deleteUserCustomer);
+
+module.exports = router;
